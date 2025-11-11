26566f7eff71e5f2fb1c87f02d44cfbf5307652c
Helen Brazier <helenbrazier@Helens-MacBook-Air.local>
2025-11-11 16:43:20 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import react from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Tasks';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      {/* Todays Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+});
